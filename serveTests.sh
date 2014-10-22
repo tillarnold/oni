b31388cad74562702f3488476e2aba7d80f1b601
tillarnold <throwable42@gmail.com>
2014-10-22 23:23:01 +0200

    Add beefy

--- serveTests.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p /tmp/oniTestBuild
-
-
-echo '<!doctype html ><html><head><title>Oni Test</title><style>*{-webkit-user-select: none; -moz-user-select: none;-ms-user-select: none;}</style></head><body><script src=bundle.js></script></body></html>' > /tmp/oniTestBuild/index.html
-echo 'Created index.html'
-browserify test/test.js > /tmp/oniTestBuild/bundle.js
-echo 'Built browserify bundle'
-serve /tmp/oniTestBuild/
